47a2d36e4f8176bdf43a1b39a6273a7ac82b296c
KNDinesh <127204193+KNDinesh@users.noreply.github.com>
2024-05-30 01:32:12 +0530

    Add files via upload

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 # outputs for the module
 output "subnet_ids" {
   description = "List of subnet IDs"
-  value       = aws_subnet.subnets[*].id
+  value       = var.subnets
 }
--- variables.tf
+++ variables.tf
@@ -12,9 +12,9 @@ variable "project_name" {
 variable "subnets" {
   description = "Map of subnet configurations"
   type = map(object({
-    cidr_block = string
-    az        = string
-    public    = bool
+    cidr_block  = string
+    az          = string
+    public      = bool
     environment = string
   }))
 }
